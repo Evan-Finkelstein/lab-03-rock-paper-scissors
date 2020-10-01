a3d6e48f215c1f31a67b4fa26140ea3fc4d7d8de
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-10-01 09:10:07 -0700

    fixed reset

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // import functions and grab DOM elements
 import { rockPaperScissors } from './utils.js'
+import { reset } from './utils.js'
 
 
 const playButton = document.querySelector('#play-button');
 const refreshButton = document.querySelector('#reset-button');
-const winSpan = document.querySelector('#wins-span');
-const drawSpan = document.querySelector('#draws-span');
-const lossSpan = document.querySelector('#losses-span');
-const restartSpan = document.querySelector('#restart-span');
-let wins = 0;
-let draws = 0;
-let losses = 0;
-let restarts = 0;
+
+
 // initialize state
 
 // set event listeners to update state and DOM
@@ -21,14 +16,5 @@ let restarts = 0;
 
 playButton.addEventListener('click', rockPaperScissors); 
 
-refreshButton.addEventListener('click', () => {
-    wins = 0;
-    losses = 0;
-    draws = 0;
-    winSpan.textContent = 0;
-    lossSpan.textContent = 0;
-    drawSpan.textContent = 0;
-    restarts++;
-    restartSpan.textContent = restarts;
+refreshButton.addEventListener('click', reset );
 
-})
