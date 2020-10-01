97e6c643d5a4016a13bf987cf5a56ccfc0dc396e
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-09-30 17:47:48 -0700

    added java

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 // import functions and grab DOM elements
+import { rockPaperScissors } from './utils.js'
 
+
+const playButton = document.querySelector('#play-button')
+const refreshButton = document.querySelector('#reset-button')
+const winSpan = document.querySelector('#wins-span')
+const drawSpan = document.querySelector('#draws-span')
+const lossSpan = document.querySelector('#losses-span')
+const restartSpan = document.querySelector('#restart-span')
+let wins = 0;
+let draws = 0;
+let losses = 0;
+let restarts = 0;
 // initialize state
 
 // set event listeners to update state and DOM
+
+
+
+
+playButton.addEventListener('click', rockPaperScissors) 
+
+refreshButton.addEventListener('click', () => {
+    wins = 0;
+    losses = 0;
+    draws = 0;
+    winSpan.textContent = 0;
+    lossSpan.textContent = 0;
+    drawSpan.textContent = 0;
+    restarts++;
+    restartSpan.textContent = restarts;
+
+})
---
+++ utils.js
@@ -0,0 +1,67 @@
+
+const resultSpan = document.querySelector('#result-span')
+const winSpan = document.querySelector('#wins-span')
+const drawSpan = document.querySelector('#draws-span')
+const lossSpan = document.querySelector('#losses-span')
+let wins = 0;
+let draws = 0;
+let losses = 0;
+
+
+export function computersPick(){
+    const randomNumber = Math.ceil(Math.random() * 3);
+    let computersThrow;
+
+    if(randomNumber === 1){
+        computersThrow = 'rock';
+    }
+    if(randomNumber === 2){
+        computersThrow = 'paper';
+    }
+    if (randomNumber === 3){
+        computersThrow = 'scissors';
+    }
+    return computersThrow;
+
+}
+
+export function rockPaperScissors(){
+const thrownValue = computersPick();
+const checkedRadioButton = document.querySelector(':checked');
+const userThrow = checkedRadioButton.value;
+
+if(userThrow === thrownValue){
+    draws++;
+    resultSpan.textContent= 'You tied!';
+    drawSpan.textContent = draws;
+} 
+if(userThrow === 'rock' && thrownValue === 'paper'){
+    losses++;
+    resultSpan.textContent= 'You lose!';
+    lossSpan.textContent = losses;
+} 
+if (userThrow === 'paper'  && thrownValue === 'scissors'){
+    losses++;
+    resultSpan.textContent= 'You lose!';
+    lossSpan.textContent = losses;
+} 
+if(userThrow === 'scissors' && thrownValue === 'rock'){
+    losses++;
+    resultSpan.textContent = 'You lose!';
+    lossSpan.textContent = losses;
+} 
+if(userThrow === 'rock' && thrownValue === 'scissors'){
+    wins++;
+    resultSpan.textContent= 'You win!';
+   winSpan.textContent = wins;
+} 
+if(userThrow === 'paper' && thrownValue === 'rock'){
+    wins++;
+    resultSpan.textContent= 'You win!';
+   winSpan.textContent = wins;
+} 
+if(userThrow === 'scissors' && thrownValue === 'paper'){
+    wins++;
+    resultSpan.textContent= 'You win!';
+    winSpan.textContent = wins;
+} }
